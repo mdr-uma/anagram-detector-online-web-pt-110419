cb27f8728e3265db7ff89004a0720f975bcdc7c7
Uma Manandhar <um_2003_8@hotmail.com>
2019-12-10 01:09:07 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    array.select do |w|
+      (@word.split("").sort) == (w.split("").sort)
+      #binding.pry
+    end
+  end
+end
